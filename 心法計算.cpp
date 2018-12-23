d9d61a24a88040c7ca321a34ace464df26b6c2bf
SpeedZhang <43335357+SpeedZhang@users.noreply.github.com>
2018-12-23 14:58:34 +0800

    Create 心法計算.cpp

---
+++ 心法計算.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void ararr(int k,int c,int a[])
+{
+    int n[5]={44,151,26,45,78};
+    int m[5]={44,541,312,545,845};
+for(int i=0;i<5;i++)
+    {
+        a[0]=n[i];
+        a[1]=m[i];
+        if(i==0)
+        {
+            printf("力道:");
+        }else if(i==1)
+        {
+            printf("氣勁:");
+        }else if(i==2)
+        {
+            printf("根骨:");
+        }else if(i==3)
+        {
+            printf("洞察:");
+        }else if(i==4)
+        {
+            printf("身法:");
+        }
+       printf("%d\n",a[k]+(a[c])/2);
+    }
+}
+int main()
+{
+    int a[38];
+
+    int k,c;
+    scanf("%d %d",&k,&c);
+    ararr(k,c,a);
+}
